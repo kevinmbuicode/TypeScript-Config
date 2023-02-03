5c147612af962a3e8460d8d8a720382cd619eb31
kevinmbuicode <kevinmushan@gmail.com>
2023-02-03 14:30:37 +0300

    feat: typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let id = 24;
+console.log("ID:", id);
+const user = {
+    id: 1,
+    name: "John"
+};
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+let id: number = 24;
+
+console.log("ID:", id)
+
+// Objects
+type User = {
+    id: number,
+    name: String
+}
+
+const user: User = {
+    id: 1,
+    name: "John"
+}
+
+interface UserP
